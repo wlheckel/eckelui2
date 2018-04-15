03d7fb174b3f74ab245d7bb59c81fb325bc0ef80
wlheckel <wlheckel@gmail.com>
2018-04-16 03:16:53 +0800

    table

---
+++ src/components/eckTable/minix.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+export default{
+    methods: {
+        alignCls (column,row ={}){
+            // 对齐方式
+            return {}
+        },
+        isPopperShow (column){
+            return {}
+        },
+        setCellWidth (column) {
+            return {}
+        }
+    }
+}
---
+++ src/components/eckTable/table-head.vue
@@ -0,0 +1,24 @@
+<template>
+    <table>
+        <thead>
+            <tr v-for="(cols,rowIndex) in headRows" :key="rowIndex">
+                <th v-for="(column,index) in cols" :key="index"
+                    :colspan="column.colSpan"
+                    :rowspan="column.rowSpan"
+                    :class="alignCls(column)">
+                    <div>
+                        
+                    </div>
+                </th>
+            </tr>
+        </thead>
+    </table>
+</template>
+
+<script>
+    
+</script>
+
+<style lang="less" scoped>
+
+</style>
